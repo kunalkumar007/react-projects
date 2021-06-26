2699ee2d34cf3e228f691c0cd745d6b70aae3c85
Kunal Kumar <kunakumar06022003@gmail.com>
2021-06-26 15:21:26 +0530

    prock added

--- packages/projects/infinite_scroll_react_unsplash-master/client/src/components/Image.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Image({ image }) {
-  return <img className='single-photo' src={image.urls.thumb} alt='' />;
-}
--- packages/projects/infinite_scroll_react_unsplash-master/config/server.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  APPLICATION_ID: process.env.APPLICATION_ID || 'REPLACE',
-  SECRET: process.env.SECRET || 'REPLACE',
-  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
-};
